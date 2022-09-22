ff0d6d6024cd1cd6019b167dd6d8da1bc9a311de
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-09-21 18:52:11 -0700

    Add application

---
+++ README.md
@@ -0,0 +1,22 @@
+# xivsniff
+A simple packet sniffer for FFXIV.
+Produces packets as [JSON Lines](https://jsonlines.org/) data to be piped into other programs.
+Data can also be piped to a file for future processing.
+
+Requires WinPcap or an equivalent driver such as [npcap](https://npcap.com/).
+
+## Format
+Line records consist of:
+* [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) timestamp, normalized to UTC
+* Version number
+* Source IP address
+* Source port
+* Destination IP address
+* Destination port
+* Packet data as a number array
+
+### Example
+```json lines
+{"t": "2022-09-22T00:51:57.721Z", "v": 1, "src_addr": "192.168.1.155", "src_port": 55321, "dst_addr": "203.0.113.18", "dst_port": 54651, "data": [115, 111, 109, 101, 66, 79, 68, 89, 32, 111, 110, 99, 101, 32, 116, 111, 108, 100, 32, 109, 101, 32, 116, 104, 101]}
+{"t": "2022-09-22T01:02:27.648Z", "v": 1, "src_addr": "192.168.1.155", "src_port": 55321, "dst_addr": "203.0.113.18", "dst_port": 54651, "data": [119, 111, 114, 108, 100, 32, 119, 97, 115, 32, 103, 111, 110, 110, 97, 32, 114, 111, 108, 108, 32, 109, 101]}
+```
---
+++ XIVSniff/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Machina.FFXIV;
+using Machina.FFXIV.Oodle;
+using Machina.Infrastructure;
+
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+static extern IntPtr FindWindow(string lpClassName, string? lpWindowName);
+
+[DllImport("user32.dll", SetLastError = true)]
+static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+static void WriteRecord(TCPConnection connection, long epoch, byte[] data)
+{
+    // Print record to stdout
+    var record = new SniffRecord
+    {
+        Timestamp = DateTimeOffset.UtcNow,
+        Version = 1,
+        SourceAddress = new IPAddress(connection.LocalIP),
+        SourcePort = connection.LocalPort,
+        DestinationAddress = new IPAddress(connection.RemoteIP),
+        DestinationPort = connection.RemotePort,
+        Data = data.Select(b => (int)b).ToArray(),
+    };
+
+    Console.WriteLine(JsonSerializer.Serialize(record));
+}
+
+// Fetch active game instance
+var window = FindWindow("FFXIVGAME", null);
+
+// Returns the ID of the thread that created the window, which we don't care about
+_ = GetWindowThreadProcessId(window, out var pid);
+if (pid == 0)
+{
+    // We do need the process ID
+    Console.Error.WriteLine("Failed to detect game instance");
+    return 1;
+}
+
+Process proc;
+try
+{
+    proc = Process.GetProcessById(Convert.ToInt32(pid));
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Failed to retrieve game process by process ID (pid={pid})");
+    Console.Error.WriteLine(e.ToString());
+    return 1;
+}
+
+string gamePath;
+try
+{
+    var fileName = proc.MainModule?.FileName;
+    if (string.IsNullOrEmpty(fileName))
+    {
+        Console.Error.WriteLine("Failed to retrieve game path from instance");
+        return 1;
+    }
+
+    gamePath = fileName;
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine("Failed to access game process main module");
+    Console.Error.WriteLine(e.ToString());
+    return 1;
+}
+
+// Start network monitor
+var monitor = new FFXIVNetworkMonitor
+{
+    MonitorType = NetworkMonitorType.WinPCap,
+    ProcessID = pid,
+    MessageReceivedEventHandler = WriteRecord,
+    MessageSentEventHandler = WriteRecord,
+    OodleImplementation = OodleImplementation.Ffxiv,
+    OodlePath = gamePath,
+};
+
+monitor.Start();
+await Task.Delay(-1);
+
+return 0;
+
+internal class SniffRecord
+{
+    [JsonPropertyName("t")]
+    [JsonPropertyOrder(0)]
+    public DateTimeOffset Timestamp { get; set; }
+
+    [JsonPropertyName("v")]
+    [JsonPropertyOrder(1)]
+    public int Version { get; set; }
+
+    [JsonPropertyName("src_addr")]
+    [JsonPropertyOrder(2)]
+    [JsonConverter(typeof(IPAddressConverter))]
+    public IPAddress? SourceAddress { get; set; }
+
+    [JsonPropertyName("src_port")]
+    [JsonPropertyOrder(3)]
+    public int SourcePort { get; set; }
+
+    [JsonPropertyName("dst_addr")]
+    [JsonPropertyOrder(4)]
+    [JsonConverter(typeof(IPAddressConverter))]
+    public IPAddress? DestinationAddress { get; set; }
+
+    [JsonPropertyName("dst_port")]
+    [JsonPropertyOrder(5)]
+    public int DestinationPort { get; set; }
+
+    [JsonPropertyName("data")]
+    [JsonPropertyOrder(6)]
+    public int[]? Data { get; set; }
+}
+
+internal class IPAddressConverter : JsonConverter<IPAddress>
+{
+    public override IPAddress? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return IPAddress.Parse(reader.GetString()!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, IPAddress value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
